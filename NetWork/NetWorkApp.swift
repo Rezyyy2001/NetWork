e7528c5718200de17c3ae97ffcce7a995d6d468e
Rezyyy2001 <92544727+Rezyyy2001@users.noreply.github.com>
2024-09-14 23:29:18 -0400

    Initial Commit

---
+++ NetWork/NetWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetWorkApp.swift
+//  NetWork
+//
+//  Created by Rezka Yuspi on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NetWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

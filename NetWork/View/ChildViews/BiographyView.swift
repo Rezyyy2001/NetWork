910667ab2f975245fa40792c53e97801ecfc2058
rezyyy2001 <rezkatyuspi@gmail.com>
2026-03-12 19:12:14 -0400

    Created a service and VM for FriendCount

--- NetWork/View/ChildViews/BiographyView.swift
+++ NetWork/View/ChildViews/BiographyView.swift
@@ -22,8 +22,6 @@ struct BiographyView<T: UserProfileDataProvider & ObservableObject>: View {
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.gray, lineWidth: 1)
                 )
-                //.padding(.horizontal, 20)
-                //.foregroundColor(.black)
         }
     }
 }
---
+++ NetWork/ViewModel/FriendCountViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  FriendCountViewModel.swift
+//  NetWork
+//
+//  Created by Rezka Yuspi on 3/12/26.
+//
+
+import Foundation
+
+@MainActor
+final class FriendCountViewModel: ObservableObject {
+    
+    @Published var friendCount: Int = 0
+    @Published var errorMessage: String?
+    
+    private let service = FriendCountService()
+    
+    var userID: String
+    
+    init(userID: String) {
+        self.userID = userID
+    }
+    
+    func fetchFriendCount(for userID: String) {
+        Task {
+            do {
+                self.friendCount = try await service.fetchFriendCount(for: userID)
+            } catch {
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+}
+

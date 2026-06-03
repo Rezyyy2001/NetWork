a1fc28c20f5057d94e1feee2681c009f1d648944
rezyyy2001 <rezkatyuspi@gmail.com>
2026-06-03 16:55:25 -0400

    fetchUserPosts finds fields with the userID and extracts the data

--- NetWork/Features/Post/PostService.swift
+++ NetWork/Features/Post/PostService.swift
@@ -55,4 +55,28 @@ struct PostService: Sendable {
             return HitPost(id: doc.documentID, userID: userID, posterName: posterName, posterUTR: posterUTR, posterUSTA: posterUSTA, location: location, city: city, date: date, extraInfo: extraInfo, numberOfPeople: numberOfPeople, isPublic: isPublic)
         }
     }
+    
+    func fetchUserPosts(userID: String) async throws -> [HitPost] {
+        guard let snapshot = try? await db.collection(FirestoreKeys.Collections.posts)
+            .whereField(FirestoreKeys.PostFields.userID, isEqualTo: userID)
+            .getDocuments()
+        else { return [] }
+        
+        return snapshot.documents.compactMap { doc in
+        
+            let userID = doc.data()[FirestoreKeys.PostFields.userID] as? String ?? ""
+            let posterName = doc.data()[FirestoreKeys.PostFields.posterName] as? String ?? ""
+            let posterUTR = doc.data()[FirestoreKeys.PostFields.posterUTR] as? Double
+            let posterUSTA = doc.data()[FirestoreKeys.PostFields.posterUSTA] as? Double
+            let location = doc.data()[FirestoreKeys.PostFields.location] as? String ?? ""
+            let city = doc.data()[FirestoreKeys.PostFields.city] as? String ?? ""
+            let timestamp = doc.data()[FirestoreKeys.PostFields.date] as? Timestamp
+            let date = timestamp?.dateValue() ?? Date()
+            let extraInfo = doc.data()[FirestoreKeys.PostFields.extraInfo] as? String ?? ""
+            let numberOfPeople = doc.data()[FirestoreKeys.PostFields.numberOfPeople] as? Int ?? 1
+            let isPublic = doc.data()[FirestoreKeys.PostFields.isPublic] as? Bool ?? true
+            
+            return HitPost(id: doc.documentID, userID: userID, posterName: posterName, posterUTR: posterUTR, posterUSTA: posterUSTA, location: location, city: city, date: date, extraInfo: extraInfo, numberOfPeople: numberOfPeople, isPublic: isPublic)
+        }
+    }
 }

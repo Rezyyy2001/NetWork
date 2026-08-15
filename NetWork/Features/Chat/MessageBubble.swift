9dab392748c3e43dfce603e1eeae866e4291afff
rezyyy2001 <rezkatyuspi@gmail.com>
2026-08-15 19:01:54 -0400

    For now it will show the full card instead of the scaled version

--- NetWork/Features/Chat/MessageBubble.swift
+++ NetWork/Features/Chat/MessageBubble.swift
@@ -13,16 +13,18 @@ struct MessageBubble: View {
     let isCurrentUser: Bool // for alignment we need to know what text belongs to who
     let card: BusinessCard?
 
+    //TODO: PUBSUB
     var body: some View {
         HStack {
             // Basically stating if its the current user, add space
             if isCurrentUser { Spacer() }
 
-            VStack {
+            VStack(alignment: isCurrentUser ? .trailing : .leading) {
                 if let card = card {
-                    BusinessCardView(card: card)
-                        .scaleEffect(0.5)
-                        .frame(height: 200)
+                    HStack {
+                        Spacer()
+                        BusinessCardView(card: card)
+                    }
                 }
                 // makes the text in a bubble
                 Text(message.text)
@@ -30,10 +32,7 @@ struct MessageBubble: View {
                     .background(isCurrentUser ? Color.blue : Color.gray.opacity(0.3))
                     .foregroundColor(isCurrentUser ? .white : .black)
                     .cornerRadius(16)
-                    .frame(maxWidth: 250, alignment: isCurrentUser ? .trailing : .leading)
-                    .padding(isCurrentUser ? .leading : .trailing, 40)
                     .padding(.horizontal, 10)
-                    .padding(.vertical, 4)
             }
             if !isCurrentUser { Spacer() }
         }

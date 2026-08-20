78338f43a10e472e93e571630cd41ffb0c666fd6
rezyyy2001 <rezkatyuspi@gmail.com>
2026-08-20 19:49:44 -0400

    move userprofile firestore mapping out of Core Model

--- NetWork/Core/Models/UserProfile.swift
+++ NetWork/Core/Models/UserProfile.swift
@@ -5,11 +5,10 @@
 //  Created by Rezka Yuspi on 3/10/26.
 //
 
-import Swift
-import Firebase
+import Foundation
 
 struct UserProfile {
-    
+
     let name: String
     let UTR: Double
     let USTA: Double
@@ -17,24 +16,10 @@ struct UserProfile {
     let bio: String?
     let birthday: Date?
     let profilePictureURL: String?
-    
+
     var age: Int? {
             guard let birthday = birthday else { return nil }
             return birthday.age
-    }
-    
-    static func from(_ data: [String: Any]) -> UserProfile {
-        let timestamp = data[FirestoreKeys.UserFields.birthday] as? Timestamp
-        
-        return UserProfile(
-            name: data[FirestoreKeys.UserFields.name] as? String ?? "",
-            UTR: data[FirestoreKeys.UserFields.utr] as? Double ?? 0.0,
-            USTA: data[FirestoreKeys.UserFields.usta] as? Double ?? 0.0,
-            usualSpot: data[FirestoreKeys.UserFields.usualSpot] as? String,
-            bio: data[FirestoreKeys.UserFields.bio] as? String,
-            birthday: timestamp?.dateValue(),
-            profilePictureURL: data[FirestoreKeys.UserFields.profilePictureURL] as? String
-        )
     }
 }
 
